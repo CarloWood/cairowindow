824da9fbf3f6308d5d31f49bc8c3dd802f55cc7d
Carlo Wood <carlo@alinoe.com>
2024-03-28 16:09:10 +0100

    No need to specify a struct body that is never used.

--- symbolic/multiply_fwd.h
+++ symbolic/multiply_fwd.h
@@ -14,10 +14,7 @@ enum IsProduct
 template<Expression E1, Expression E2, IsProduct is_product =
   ((is_constant_v<E1> && !is_constant_v<E2>) || is_symbol_v<E1> || is_power_v<E1> || is_product_v<E1>) &&
   ((is_constant_v<E2> && !is_constant_v<E1>) || is_symbol_v<E2> || is_power_v<E2> || is_product_v<E2>) ? isProduct : not_a_Product>
-struct multiply
-{
-  using type = void;
-};
+struct multiply;
 
 template<Expression E1, Expression E2>
 using multiply_t = typename multiply<E1, E2>::type;
