31f37eed903d0e6b766cd2f50b17213d34c0837f
Carlo Wood <carlo@alinoe.com>
2024-06-15 14:22:20 +0200

    Add comments to histogram.cxx.

--- tests/histogram.cxx
+++ tests/histogram.cxx
@@ -13,7 +13,7 @@
 using HorizontalDirection = gradient_descent::HorizontalDirection;
 using VerticalDirection = gradient_descent::VerticalDirection;
 
-constexpr int number_of_test_runs = 1; //000;
+constexpr int number_of_test_runs = 2; //000;
 constexpr int min_number_of_extremes = 1;
 constexpr int max_number_of_extremes = 11;
 constexpr int max_step_size = 100;
@@ -247,36 +247,19 @@ class AcceleratedGradientDescent
   }
 };
 
-bool AcceleratedGradientDescent::handle_local_extreme(Weight& w)
-{
-  // Store it as an extreme.
-  extremes_[w] = LocalExtreme{w};
-  extremes_type::iterator new_extreme = &extremes_[w];
-
-  // Keep track of the best minimum so far; or abort if this minimum isn't better then one found before.
-  if (Histogram::is_minimum(w))
-  {
-    if (best_minimum_ == extremes_.end() || histogram_[best_minimum_->w()] > histogram_[new_extreme->w()])
-      best_minimum_ = new_extreme;
-    if (new_extreme != best_minimum_)
-      return false;
-  }
-
-  if (!do_step(w))
-    return false;
-
-  // w was successfully updated.
-  return true;
-}
-
-bool AcceleratedGradientDescent::operator()(Weight& w, int height)
-{
-  // Every sample is a local extreme here.
-  if (!handle_local_extreme(w))
-    return handle_abort_hdirection(w);
-  return true;        // w was successfully updated by handle_local_extreme.
-}
-
+// lowest_side
+//
+// Returns HorizontalDirection::undecided, HorizontalDirection::left or HorizontalDirection::right.
+//
+// In the real code, a fourth degree polynomial is fitted whenever an extreme was found.
+// This fourth degree polynomial has three extremes; the one in the middle matches the extreme
+// that was just found and the two on the left and right approximate the height and position
+// of the extremes of opposite type (minimum <--> maximum). The direction chosen is then the
+// direction in which that extreme has the lowest value (for both maximum and minimum).
+//
+// In this test code we simply use the actual "extremes" left and right of the current one:
+// the values of the histogram left and right of the current index w.
+//
 HorizontalDirection AcceleratedGradientDescent::lowest_side(Weight const& w)
 {
   HorizontalDirection result = HorizontalDirection::undecided;
@@ -290,6 +273,22 @@ HorizontalDirection AcceleratedGradientDescent::lowest_side(Weight const& w)
   return result;
 }
 
+// jump
+//
+// Returns the distance that we would jump if going direction hdirection_.
+// If hdirection_ is left then a negative value is returned, if hdirection_ is right a positive value,
+// so that simply adding the returned value to w gives the new w.
+//
+// Returns 0 if there is no jump possible in that direction (because the immediate neighbor was already visited).
+//
+// In the real code a polynomial is fitted and extra polated to predict the next extreme
+// to jump to. Since this can completely miss the target, those jumps are generated as
+// random values in this test. However, we should never jump past an already sampled point.
+// To mimic this here we never jump past an already visited "extreme" (histogram index).
+// But, in order not to lose the randomness, the returned value isn't clamped to the nearest
+// visited extreme, instead we half the jumping distance until it falls within the allowed
+// range.
+//
 int AcceleratedGradientDescent::jump(Weight const& w)
 {
   // Find nearest neighbor that was already visited.
@@ -316,6 +315,27 @@ int AcceleratedGradientDescent::jump(Weight const& w)
   return step;
 }
 
+// do_step
+//
+// Returns true upon success, false otherwise.
+//
+// Execute a 'step': go from the current extreme to the next.
+// The algorithm here is as follows:
+// - pick a direction: if the current extreme is already explored
+//   left and right, then return false. Otherwise if the current
+//   extreme is already explored to the left, go right. Otherwise,
+//   if the current extreme is already explored to the right, go left.
+//   Otherwise, go to the "lowest side".
+//
+// - call jump. If that returns -1, 0 or 1, mark the extreme as
+//   having been explored in this direction and try the other
+//   side (if that wasn't already explored) or abort (return false).
+//
+// The reasoning here is as follows: if jump return -1 or 1 we jump
+// to the immediate neighbor, therefore we can never go in that
+// direction again. And if it return 0 we can already never go into
+// that direction, so don't try again in the future.
+//
 bool AcceleratedGradientDescent::do_step(Weight& w)
 {
   int step;
@@ -353,6 +373,46 @@ bool AcceleratedGradientDescent::do_step(Weight& w)
   return true;
 }
 
+// handle_local_extreme
+//
+// Returns true upon success and false upon failure.
+//
+// In this test, every sample is an extreme (each column of the
+// histogram represents an extreme). Nevertheless we store them
+// in the order that we're visiting them.
+//
+// This function is broken. The idea here we already are exploring
+// a certain horizontal direction relative to the best minimum that
+// we found so far, and keep going in that direction until we find
+// a minimum that is NOT better.
+//
+// If you assume that we find all extremes exactly in order,
+// then this algorithm works. But it is possible that the best
+// minimum is jumped over (or is it?).
+bool AcceleratedGradientDescent::handle_local_extreme(Weight& w)
+{
+  // Store it as an extreme.
+  extremes_[w] = LocalExtreme{w};
+  extremes_type::iterator new_extreme = &extremes_[w];
+
+  // Keep track of the best minimum so far; or abort if this minimum isn't better then one found before.
+  if (Histogram::is_minimum(w))
+  {
+    if (best_minimum_ == extremes_.end() || histogram_[best_minimum_->w()] > histogram_[new_extreme->w()])
+      best_minimum_ = new_extreme;
+    if (new_extreme != best_minimum_)
+      return false;
+  }
+
+  // Go to the next extreme.
+  return do_step(w);
+}
+
+// handle_abort_hdirection
+//
+// Called when we can't go further in the current hdirection.
+// In this case we want to jump to the best minimum so far
+// and check that we explored both sides of it already.
 bool AcceleratedGradientDescent::handle_abort_hdirection(Weight& w)
 {
   // Has a minimum been found at all?
@@ -363,24 +423,28 @@ bool AcceleratedGradientDescent::handle_abort_hdirection(Weight& w)
   w = best_minimum_->w();
 
   // Go to the next extreme.
-  if (!do_step(w))
-    return false;
+  return do_step(w);
+}
 
-  // w was successfully updated.
-  return true;
+bool AcceleratedGradientDescent::operator()(Weight& w, int height)
+{
+  // Every sample is a local extreme here.
+  if (!handle_local_extreme(w))
+    return handle_abort_hdirection(w);
+  return true;        // w was successfully updated by handle_local_extreme.
 }
 
 int minimum(Histogram const& histogram)
 {
-  // * start at starting_position_.
-  int w = histogram.starting_position();
-
   // Create algorithm object.
   AcceleratedGradientDescent agd(histogram);
 
+  // * start at starting_position_.
+  Weight w{histogram.starting_position()};
+
   // Loop over iterations of w.
   std::cout << w << std::flush;
-  for (Weight w(histogram.starting_position());;)
+  for (;;)
   {
     // Replace w with a new point until the global minimum has been reached.
     if (!agd(w, histogram[w]))
@@ -398,7 +462,7 @@ int main()
 {
   Debug(NAMESPACE_DEBUG::init());
 
-  std::seed_seq seed{1, 2, 3, 4, 8};
+  std::seed_seq seed{1, 2, 3, 4, 10};
   std::mt19937 generator(seed);
 
   // Generate 1000 tests.
