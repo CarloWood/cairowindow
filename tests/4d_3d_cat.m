a5fc41fe00dae6c0fca59e770ce7bc94a668f098
Carlo Wood <carlo@alinoe.com>
2024-09-19 13:58:44 +0200

    Update 4d_3d_cat.m

--- tests/4d_3d_cat.m
+++ tests/4d_3d_cat.m
@@ -28,18 +28,53 @@ D_sol = solve(eq1_subC, D);
 Q_final = subs(Q, {C, D}, {C_sol, D_sol});
 disp(Q_final);
 
+disp("\n1260 times the integral from x1 to x1+δ of (P(x) - Q(x))² is given by:");
 R = P - Q_final;
 error = int(R * R, x);
-
 error_x1_plus_delta = subs(error, x, x1 + delta);
 error_x1 = subs(error, x, x1);
 def_integral = error_x1_plus_delta - error_x1;
 simplified_def_integral = simplify(def_integral);
+disp(simplify(1260 * simplified_def_integral));
 
 partial_A = diff(simplified_def_integral, A);
 partial_B = diff(simplified_def_integral, B);
 
+disp("\nSetting the partial derivative with respect to A to zero gives:");
 eq3 = partial_A == 0;
-eq4 = partial_B == 0;
-
 disp(eq3);
+
+disp("\nSetting the partial derivative with respect to B to zero gives:");
+eq4 = partial_B == 0;
+disp(eq4);
+
+disp("\nFrom this we can solve A and B:");
+B_sol = solve(eq4, B);
+eq3_subB = subs(eq3, B, B_sol);
+A_sol = solve(eq3_subB, A);
+B_sol_subA = subs(B_sol, A, A_sol);
+B_sol_subA_simplified = simplify(B_sol_subA);
+disp("\nA =");
+disp(A_sol);
+disp("\nB =");
+disp(B_sol_subA_simplified)
+
+ddQ = diff(dQ, x);
+ddQ_x1 = subs(ddQ, x, x1);
+ddQ_x1_subAB = simplify(subs(subs(ddQ_x1, A, A_sol), B, B_sol_subA));
+dddQ = diff(ddQ, x);
+dddQ_x1_subAB = simplify(subs(dddQ, A, A_sol));
+ddP = diff(dP, x);
+dddP = diff(ddP, x);
+disp("\nQ''(x1) = ");
+disp(ddQ_x1_subAB)
+disp("\nQ'''(x1) = ");
+disp(dddQ_x1_subAB)
+
+disp("\nNote that P''(x1) =");
+ddP_x1 = simplify(subs(ddP, x, x1));
+disp(ddP_x1);
+
+disp("\nAnd P'''(x1) =");
+dddP_x1 = simplify(subs(dddP, x, x1));
+disp(dddP_x1);
